75d72ca1fa16c2ec9b31e082429fef8b3b09b178
Meghanadesai <meghanadesai1996@gmail.com>
2023-12-30 20:00:12 +0100

    Implemented internalization commit

--- src/main/java/com/rest/webservice/restfulwebservice/helloworld/HelloWorldController.java
+++ src/main/java/com/rest/webservice/restfulwebservice/helloworld/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.rest.webservice.restfulwebservice.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
 	
 	@GetMapping("/hello-world")
 	public String hello() {
@@ -20,5 +30,11 @@ public class HelloWorldController {
 	@GetMapping("/hello-world/path-variable/{name}")
 	public HellowWorldBean helloWorldBean(@PathVariable String name) {
 		return new HellowWorldBean(String.format("HelloWorld, %s", name));
+	}
+	
+	@GetMapping("/hello-world-internationalized")
+	public String helloInternationalized() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 }
