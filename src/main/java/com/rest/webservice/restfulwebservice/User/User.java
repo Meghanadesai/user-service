bfcfd0014a2a3732b48b852af46b908e36cbb71d
Meghanadesai <meghanadesai1996@gmail.com>
2024-02-13 19:06:12 +0100

    Jpa and Hibernate changes commit

--- src/main/java/com/rest/webservice/restfulwebservice/User/User.java
+++ src/main/java/com/rest/webservice/restfulwebservice/User/User.java
@@ -4,12 +4,22 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name = "user_details")
 public class User {
 	
+	protected User() {
+		
+	}
+	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	
 	@Size(min = 3, message = "Name should have atleast 3 characters")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details (birth_date,id,name) values(current_date(),100,'Meghana');
+insert into user_details (birth_date,id,name) values(current_date(),101,'Padma');
+insert into user_details (birth_date,id,name) values(current_date(),102,'KrishnaMurthy');
